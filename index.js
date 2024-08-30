9ff10007b4f2073c9e1f377f8be13a09e7242b73
prakhargangwar120 <113546743+prakhargangwar120@users.noreply.github.com>
2024-08-30 12:20:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,40 @@
-MIT License
 
-Copyright (c) 2024 Gevendra Sahu
+  const inputBox = document.getElementById('input-box');
+const listContainer = document.getElementById('list-container');
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+function addTask() {
+    if(inputBox.value == ''){
+        alert("Please enter a Task");
+        return;
+    } else {
+        const li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
 
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
+        const span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = ""
+    saveData();
+}
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
+listContainer.addEventListener('click', function(e){
+    if(e.target.tagName == 'LI'){
+        e.target.classList.toggle('checked');
+        saveData();
+    } else if(e.target.tagName == 'SPAN') {
+        e.target.parentElement.remove()
+        saveData();
+    }
+});
+
+function saveData() {
+    localStorage.setItem("data" , listContainer.innerHTML);
+}
+
+function showTask() {
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+
+showTask();
